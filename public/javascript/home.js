42ab1be48ede87e62936ff12f0d917999fa0bd57
rolandOrtiz28 <gwapohejunjo@gmail.com>
2025-01-03 10:50:23 +0700

    update

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -34,29 +34,50 @@ gsap.utils.toArray(".infographic-item").forEach((item, index) => {
   gsap.from(item, {
     y: 50,
     opacity: 0,
-    duration: 0.8, // Shorter duration
+    duration: 0.8, 
     ease: "power2.out",
     scrollTrigger: {
       trigger: item.closest(".infographic-row"),
       start: "top 80%",
       end: "top 50%",
-      scrub: 0.5, // Reduced scrub intensity
+      scrub: 0.5, 
+  
     },
   });
 });
 
+gsap.utils.toArray(".gallery-column img").forEach((img) => {
+  gsap.fromTo(
+    img,
+    { opacity: 0, scale: 0.9 },
+    {
+      opacity: 1,
+      scale: 1,
+      duration: 1,
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger: img,
+        start: "top 90%",
+        toggleActions: "play none none none",
+       
+      },
+    }
+  );
+});
+
+// Smooth zoom for the entire project gallery
 gsap.to(".project-gallery", {
-  scale: 0.8, // Zoom in effect
+  scale: 0.8,
   scrollTrigger: {
     trigger: ".project-gallery",
     start: "top 80%",
     end: "bottom 20%",
-    scrub: true, // Smooth zoom effect on scroll
+    scrub: true,
   },
 });
 
 gsap.to(".project-gallery", {
-  scale: 1, // Zoom out effect
+  scale: 1,
   scrollTrigger: {
     trigger: ".project-gallery",
     start: "bottom 80%",
@@ -65,6 +86,41 @@ gsap.to(".project-gallery", {
   },
 });
 
+document.querySelectorAll(".gallery-column").forEach((column) => {
+  column.addEventListener("mouseenter", () => {
+    column.style.animationPlayState = "paused"; // Pause animation
+  });
+
+  column.addEventListener("mouseleave", () => {
+    column.style.animationPlayState = "running"; // Resume animation
+  });
+});
+
+// JavaScript to reset scrolling animation
+document.querySelectorAll(".gallery-content").forEach((content) => {
+  content.addEventListener("animationiteration", () => {
+    content.style.animation = "none";
+    requestAnimationFrame(() => {
+      content.style.animation = "";
+    });
+  });
+});
+
+gsap.utils.toArray(".services-container .glass").forEach((box, index) => {
+  gsap.from(box, {
+    y: -200, // Start position above the viewport
+    opacity: 0, // Start fully transparent
+    duration: 1, // Duration of the drop animation
+    ease: "bounce.out", // Bounce effect for the drop
+    delay: index * 0.2, // Stagger the animations for each box
+    scrollTrigger: {
+      trigger: "#services", // Trigger animation when #services section comes into view
+      start: "top 80%", // Start when the section is 80% visible
+      end: "bottom 50%", // Start when the section is 80% visible
+      scrub:true
+    },
+  });
+});
 
 // Improved map loading performance
 mapboxgl.accessToken = "pk.eyJ1Ijoicm9sYW5kMjgiLCJhIjoiY201NmxhMGZ6MHNxcDJrc2c4NnNvd25weSJ9.FW6t7CihNZT10Af4V_sw-g";
